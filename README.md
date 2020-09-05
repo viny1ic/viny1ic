85ee56710cd93af1b00b78117e452719085e062e
viny1ic <vinayak.dubey2019@vitstudent.ac.in>
2020-09-06 04:54:10 +0530

    weather thing giving shit

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-### Hi there 👋
-im not quite there yet.
-give me some time
-![Visitor Count](https://profile-counter.glitch.me/viny1ic/count.svg)
+
+<h1><img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="30"/> My name is Vinayak Dubey .</h1>
+<p>I am a B.Tech CSE Student from VIT vellore, India. <br> Today is Sunday, 6 September, 4:16 am IST <br> The weather in vellore today is  °C, <i>  </i><p>
---
+++ index.js
@@ -0,0 +1,59 @@
+// index.js
+const Mustache = require('mustache');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const MUSTACHE_MAIN_DIR = './main.mustache';/**
+  * DATA is the object that contains all
+  * the data to be provided to Mustache
+  * Notice the "name" and "date" property.
+*/
+let DATA = {
+  name: 'Vinayak Dubey',
+  date: new Date().toLocaleDateString('en-IN', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Asia/Kolkata',
+  }),
+};/**
+  * A - We open 'main.mustache'
+  * B - We ask Mustache to render our file with the data
+  * C - We create a README.md file with the generated output
+  */
+
+async function setWeatherInformation() {
+    await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=vellore&appid=072e2ccd99a1a0df052481df2b434f93&units=metric`
+    )
+      .then(r => r.json())
+      .then(r => {
+        DATA.city_temperature = Math.round(r.main.temp);
+        DATA.city_weather = r.weather[0].description;
+        DATA.city_weather_icon = r.weather[0].icon;
+        DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-IN', {
+          hour: '2-digit',
+          minute: '2-digit',
+          timeZone: 'Asia/Kolkata',
+        });
+        DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-IN', {
+          hour: '2-digit',
+          minute: '2-digit',
+          timeZone: 'Asia/kolkata',
+        });
+      });
+  }
+
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+
+setWeatherInformation();
+
+generateReadMe();
